277389707b393e2754b453fab5204bd9afae0e7f
Junior <juniorantoniocalvomontanez@gmail.com>
2021-01-05 00:20:04 -0500

    Trabajando en gráficas mucho mas simples

--- timeSeries.R
+++ timeSeries.R
@@ -28,8 +28,31 @@ grafCruceta<-plot(Cruceta.ts,
                     main = "Estación Meteorológica de Cruceta",
                     sub="Fuente: Datos PISCO SENAMHI")
 
-colnames(data_long_tati)
-CURVAN
+#Curvan=3,ElTablazo=4, Hualtaco=5,Malingas=6 y 7, TamboGrande=8,SanJoaquin=9,Tejedores=10
+#Chulucanas=11,HaciendaYapatera=12,
+Huapalas<-data_long_tati[,12]
+Huapalas.ts<-ts(Huapalas, start = 1981, frequency = 12)
+#view(SanIsidro.ts)
+grafHuapalas<-plot(Huapalas.ts,
+                  xlab="Año (1981 al 2016)",
+                  ylab="Precipitaciones mensuales",
+                  type="S", 
+                  col="blue",
+                  lwd="2.5",
+                  main = "Estación Meteorológica de Huapalas",
+                  sub="Fuente: Datos PISCO SENAMHI")
+        
+#"HUAPALAS"          "SAN PEDRO"         "SANTO DOMINGO"    
+#[16] "SANTO DOMINGO"     "MIRAFLORES"        "PIURA"            
+#[19] "ALTAMIZA"          "CHALACO"           "PALO BLANCO"      
+#[22] "PASAPAMPA"         "MORROPON"          "CORRAL DEL MEDIO" 
+#[25] "PALTASHACO"        "SAN MIGUEL"        "VIRREY"           
+#[28] "HACIENDA PABUR"    "CANCHAQUE"         "HUANCABAMBA"      
+#[31] "HACIENDA BIGOTE"   "BARRIOS"           "MONTEGRANDE"      
+#[34] "MALACASI"          "MALACASI"          "CHIGNIA"          
+#[37] "PIRGAS"            "BERNAL"            "CHUSIS"           
+#[40] "LAGUNA RAMON"#
+
 plot(x <- sort(rnorm(47)), type = "s", main = "plot(x, type = \"s\")")
 points(x, cex = .5, col = "dark red")
 plot(table(rpois(100, 5)), type = "h", col = "red", lwd = 10,

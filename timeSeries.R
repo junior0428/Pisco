23889551676c0ee3a54da55c766e25742ae4f0fa
Junior <juniorantoniocalvomontanez@gmail.com>
2021-01-05 00:21:34 -0500

    Trabajando en gráficas mas simples

--- timeSeries.R
+++ timeSeries.R
@@ -30,7 +30,7 @@ grafCruceta<-plot(Cruceta.ts,
 
 #Curvan=3,ElTablazo=4, Hualtaco=5,Malingas=6 y 7, TamboGrande=8,SanJoaquin=9,Tejedores=10
 #Chulucanas=11,HaciendaYapatera=12,
-Huapalas<-data_long_tati[,12]
+Huapalas<-data_long_tati[,13]
 Huapalas.ts<-ts(Huapalas, start = 1981, frequency = 12)
 #view(SanIsidro.ts)
 grafHuapalas<-plot(Huapalas.ts,
@@ -41,8 +41,9 @@ grafHuapalas<-plot(Huapalas.ts,
                   lwd="2.5",
                   main = "Estación Meteorológica de Huapalas",
                   sub="Fuente: Datos PISCO SENAMHI")
-        
-#"HUAPALAS"          "SAN PEDRO"         "SANTO DOMINGO"    
+ 
+    
+#   "SAN PEDRO"         "SANTO DOMINGO"    
 #[16] "SANTO DOMINGO"     "MIRAFLORES"        "PIURA"            
 #[19] "ALTAMIZA"          "CHALACO"           "PALO BLANCO"      
 #[22] "PASAPAMPA"         "MORROPON"          "CORRAL DEL MEDIO" 
@@ -52,6 +53,22 @@ grafHuapalas<-plot(Huapalas.ts,
 #[34] "MALACASI"          "MALACASI"          "CHIGNIA"          
 #[37] "PIRGAS"            "BERNAL"            "CHUSIS"           
 #[40] "LAGUNA RAMON"#
+Estaciones<-data_long_tati[,1:40]
+funcion<-funcion(Estaciones){
+            Estaciones[i].ts<-ts(Huapalas, start = 1981, frequency = 12)
+            #view(SanIsidro.ts)
+            grafHuapalas<-plot(Huapalas.ts,
+                               xlab="Año (1981 al 2016)",
+                               ylab="Precipitaciones mensuales",
+                               type="S", 
+                               col="blue",
+                               lwd="2.5",
+                               main = "Estación Meteorológica de Huapalas",
+                               sub="Fuente: Datos PISCO SENAMHI")
+}  
+
+
+
 
 plot(x <- sort(rnorm(47)), type = "s", main = "plot(x, type = \"s\")")
 points(x, cex = .5, col = "dark red")
